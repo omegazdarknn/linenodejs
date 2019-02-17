3dfc4eb365c6f0a37385b56492d7545dccdcda2e
Supakrit Thiamsuwan <omegazdarknn@yahoo.com>
2019-02-17 12:32:16 +0700

    make it better

---
+++ index.js
@@ -0,0 +1,10 @@
+const server = require('express');
+const PORT = process.env.PORT || 9999;
+const request = require('request');
+const bodyParser = require('body-parser');
+
+server()
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({ extended: false}))
+    .get('/', (req, res) => res.send(`Hi there! This is a nodejs-line-api running on PORT: ${ PORT }`))
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
