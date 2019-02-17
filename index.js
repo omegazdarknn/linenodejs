f10aabf529f4ee3064ad5489aaa800e4550944a6
Supakrit Thiamsuwan <omegazdarknn@yahoo.com>
2019-02-17 12:43:09 +0700

    make it better

--- index.js
+++ index.js
@@ -8,3 +8,17 @@ server()
     .use(bodyParser.urlencoded({ extended: false}))
     .get('/', (req, res) => res.send(`Hi there! This is a nodejs-line-api running on PORT: ${ PORT }`))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+    .post('/webhook', function (req, res) {
+        let replyToken = req.body.events[0].replyToken;
+        let msg = req.body.events[0].message.text;
+
+        console.log(`Message token : ${ replyToken }`);
+        console.log(`Message from chat : ${ msg }`);
+
+        res.json({
+            status: 200,
+            message: `Webhook is working!`
+        });
+    })
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
